9b0c0cfc84de7666063a679e99a27b42cad63119
The User <user@email.com>
2025-11-19 16:29:20 -0300

    stanley; test php html

--- uploadQR.php
+++ uploadQR.php
@@ -25,17 +25,18 @@
 
 	//Check IF the upload time less than 5 minutes & delete in that case
 	$query = "DELETE FROM tblFiles WHERE TIMESTAMPDIFF(MINUTE, uploadDate, NOW()) > 5;";
-	echo $query;
 	$success = $db->query($query);
-	
 	//Check success
 	if (!$success) header("Location: logout.php");
 
-	echo "$IPSERVER$fileName";
-	$qrCreated=eval("qrencode -m 3 -s 50 -d 600 -l M -o /$UPLOADFILES/qr/$randomFilename.png '$IPSERVER$UPLOADFILES'");
+	$command = "qrencode -m 3 -s 50 -d 600 -l M -o .$UPLOADFILES/qr/$randomFilename.png '$IPSERVER/QRTransfert$fileName'";
+	exec($command,$output,$returnCode);
+	echo $command, $qrCreated;
+	printf($output);
 
-	if ($qrCreated) echo "<img src='/$UPLOADFILES/qr/$randomFilename.png' alt='$randomFilename.png'/>";
-	else header("Location: logout.php");
+	if ($returnCode == 0) 
+	header("Location: index.html?qrcode=<img src='.$UPLOADFILES/qr/$randomFilename.png' alt='$randomFilename.png'/>");
+	// else header("Location: logout.php");
 
 	// Function : generate random filename
 	function generateRandomFilename($length=20) {
